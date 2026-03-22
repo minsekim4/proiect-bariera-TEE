293fed7b46f3585bb0f641c5ab25852da1dfa955
minsekim4 <minsekim4@gmail.com>
2026-03-22 16:02:00 +0200

    Update interface_output.sv

--- interface_output.sv
+++ interface_output.sv
@@ -4,20 +4,20 @@
 interface interface_output(input logic clk,reset);
   
   //declaring the signals
-  logic 	  stare_bariera;
+  logic 	    stare_bariera   ;
   logic [4:0] nr_locuri_libere;
-  logic		  parcare_plina;
-  logic 	  parcare_goala;
+  logic		    parcare_plina   ;
+  logic 	    parcare_goala   ;
   //semnalele din clocking block sunt sincrone cu frontul crescator de ceas
   //driver clocking block
 
   //monitor clocking block
   clocking monitor_cb @(posedge clk);
     default input #1 output #1;
-    input stare_bariera;
-    input nr_locuri_libere; 
-    input parcare_plina;
-    input parcare_goala;
+    input stare_bariera       ;
+    input nr_locuri_libere    ; 
+    input parcare_plina       ;
+    input parcare_goala       ;
   endclocking
   
   //monitor modport  
